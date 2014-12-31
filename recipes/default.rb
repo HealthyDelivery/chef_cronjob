b57dcffa41e51844bf256624e96331a50b9a3e5d
Daniel Nelson <daniel@populr.me>
2014-12-31 15:46:54 -0600

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: chef_cronjob
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node[:deploy].each do |application, deploy|
+  cron 'cronjobs_rake' do
+    minute '*/5'
+    command "sudo su deploy && cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} NEWRELIC_DISPATCHER='rake' PATH='/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin' bundle exec rake cron"
+  end
+end
